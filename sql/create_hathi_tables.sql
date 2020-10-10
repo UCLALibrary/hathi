dfd1c4aaf0098dff5ad941fee8e5fed633abe0c3
Andy Kohler <akohler@library.ucla.edu>
2020-10-09 17:42:16 -0700

    Update database table/index DDL to what is used now

---
+++ sql/create_hathi_tables.sql
@@ -0,0 +1,31 @@
+-- Permanent table used by web services
+create table vger_report.hathi_overlap (
+  oclc_number varchar2(15) not null
+, bib_id int not null
+, hathi_bib_key varchar2(10) not null
+, item_type varchar2(6) not null
+, access_lvl varchar2(5)
+, rights_code varchar2(20)
+)
+;
+create index vger_report.ix_hathi_overlap on vger_report.hathi_overlap (bib_id, hathi_bib_key);
+grant select on vger_report.hathi_overlap to ucla_preaddb;
+
+-- Temporary tables for imports/updates
+create table vger_report.tmp_hathi_import (
+  oclc_number varchar2(15) not null
+, hathi_bib_key varchar2(10) not null
+)
+;
+create index vger_report.ix_tmp_hathi_import on vger_report.tmp_hathi_import (oclc_number);
+
+create table vger_report.tmp_hathi_overlap (
+  oclc_number varchar2(15) not null
+, bib_id int not null
+, item_type varchar2(6) not null
+, access_lvl varchar2(5)
+, rights_code varchar2(20)
+)
+;
+create index vger_report.ix_tmp_hathi_overlap on vger_report.tmp_hathi_overlap (oclc_number);
+  
